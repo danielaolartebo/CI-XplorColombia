d59b67e4838b5538040fae70c92ad1140b085c9d
Daniela Olarte <danielaolarte123@gmail.com>
2024-04-09 22:07:42 -0500

    changes

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -37,23 +37,113 @@ export default function Welcome() {
   
   
             {/* CREATE TRIP PAGE STARTS */}
-            <div className="aboutus-container">
-              <div className="deals-subtitle" id="create">
-                    Create trip
+            <div className="aboutus-container" id="create">
+                  <div className="container-centered">
+                    <div className="deals-subtitle">
+                      Create trip
+                    </div>
+                    <div className="items-container">
+                      <div className="text-aboutus">
+                        {/* ---------------------- ID AND USER NAME ----------------------------*/}
+                        <div className="cont-subtitle">
+                          <div className="items-half1">
+                            <input className="form-items1" placeholder="ID" type="id" name="id" required/>
+                          </div>
+                          <div className="items-half2">
+                            <input className="form-items2" placeholder="Full Name" type="name" name="name" required/>
+                          </div>
+                        </div>
+
+                        {/* ---------------------- NUMBER OF PEOPLE ----------------------------*/}
+                        <div className="cont-subtitle">
+                          <div className="items-half1">
+                              <div className="text-items">
+                                  <div className="text-design">Number of people </div>
+                              </div>
+                          </div>
+                          <div className="items-half2">
+                            <select className="dropdown-menu" id="menu"> 
+                                <option value="" disabled selected>Select option</option>
+                                <option value=""> 1 </option> 
+                                <option value=""> 2 </option> 
+                                <option value=""> 3 </option> 
+                                <option value=""> 4 </option> 
+                                <option value=""> 5 </option> 
+                                <option value=""> 6 </option> 
+                                <option value=""> 7 </option> 
+                                <option value=""> 8 </option> 
+                                <option value=""> 9 </option> 
+                                <option value=""> 10 </option> 
+                            </select>
+                          </div>
+                        </div>
+
+                        {/* ---------------------- DESTINATIONS ----------------------------*/}
+                        <div className="cont-subtitle">
+                          <div className="items-half1">
+                              <div className="text-items">
+                                  <div className="text-design">Destination 1 </div>
+                              </div>
+                          </div>
+                          <div className="items-half2">
+                            <select className="dropdown-items" id="menu"> 
+                                <option value="" disabled selected> Select trip</option>
+                                <option value=""> Caño Cristales </option> 
+                                <option value=""> Parque Tayrona </option> 
+                                <option value=""> Ciudad Perdida </option> 
+                                <option value=""> Desierto de la Tatacoa </option> 
+                                <option value=""> Parque del Café </option> 
+                                <option value=""> Piedra del Peñol </option> 
+                                <option value=""> Nevado del Ruiz </option> 
+                                <option value=""> Valle del Cocora </option>
+                            </select>
+                           </div>
+                          </div>
+                           {/* ---------------------- ADD BUTTON ----------------------------*/}
+                           <div className="cont-subtitle">
+                          <div className="items-half1">
+                              
+                          </div>
+                          <div className="items-half2">
+                              <li className="button-14"><a href="#" className="button"> Add  </a></li>
+                              <li className="button-14"><a href="#" className="button">Delete</a></li>
+                           </div>
+                          </div>
+                      </div>
+                </div>
               </div>
-              <div className="text-container">
-                  <div className="text-aboutus">
-                      <br/><br/>
-                      <br/><br/>
+              {/* CREATE TRIP PAGE ENDS */}
+
+              {/* PRINCING STARTS */}
+              <section>
+                <div className="cont-pricing2"> 
+                  <div className="title-price" style={{textAlign:'center'}}>Princing & Details</div>
+                  <div className="columns">
+                    <ul className="price">
+                      <li className="header">Trip name</li>
+                      <li className="grey">$ 0 COP </li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
                   </div>
-              </div>
+                  <ul className="price2">
+                    <li><a href="#" className="button">Download</a></li>
+                  </ul>
+                </div> 
+              </section>
+              {/* PRINCING ENDS */}
             </div>
-             {/* CREATE TRIP PAGE ENDS */}
+            {/* CREATE TRIP PAGE ENDS */}
   
   
             {/* DEALS PAGE STARTS */}
-            <div className="package-grid">
-              <div className="deals-subtitle" id="deals">
+            <div className="package-grid" id="deals">
+              <div className="deals-subtitle">
                   Check out our trip deals!
               </div>
               <div className="grid">
@@ -140,8 +230,8 @@ export default function Welcome() {
               {/* DEALS PAGE ENDS */}
   
               {/* MOST POPULAR PAGE STARTS */}
-                <div className="mostpop-container">
-                  <div className="deals-subtitle" id="mostpopular">
+                <div className="mostpop-container" id="mostpopular">
+                  <div className="deals-subtitle">
                     Get to know our most popular trips
                   </div>
                   <div className="progressbar-container">
